04188e296e35030a3a7dffa435af0f188fc972c3
fidan2004 <110298605+fidan2004@users.noreply.github.com>
2023-02-11 01:17:43 +0400

    Add files via upload

---
+++ Web-Api-hometask1/Web-Api-hometask1/Controllers/ValuesController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Web_Api_hometask1.Model;
+
+namespace Web_Api_hometask1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public static List<Book> _books = new List<Book>();
+
+        [HttpGet("book/get")]
+        public List<Book> GetAll()
+        {
+
+            return _books;
+        }
+
+        [HttpGet("book/get{id}")]
+        public IActionResult Get(int id)
+        {
+            Book book = _books.FirstOrDefault(m => m.Id == id);
+            if (book != null)
+                return Ok(book);
+            else
+                return NotFound();
+        }
+
+        [HttpPost("book/create")]
+        public IActionResult Create(Book book)
+        {
+            if (book.Id != null)
+            {
+                _books.Add(book);
+                return Ok(book);
+            }
+            else
+                return NotFound();
+        }
+
+        [HttpPut]
+        public IActionResult Update(int id, string bookName, decimal price, string category, string author)
+        {
+            var updated = _books.FirstOrDefault(x => x.Id == id);
+            if (updated.Id != null)
+            {
+                _books.Remove(updated);
+                updated.BookName = bookName;
+                updated.Price = price;
+                updated.Category = category;
+                updated.Author = author;
+
+                _books.Add(updated);
+                return Ok(updated);
+            }
+            else
+                return NotFound();
+
+        }
+        [HttpDelete]
+        public void Remove(int id)
+        {
+            Book book = _books.FirstOrDefault(x => x.Id == id);
+            _books.Remove(book);    
+            
+        }
+
+  }
+}
---
+++ Web-Api-hometask1/Web-Api-hometask1/Model/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace Web_Api_hometask1.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Author { get; set; }
+
+        public static int id = 0;
+        public Book( string bookName, decimal price, string category, string author)
+        {
+
+            Id = ++id;
+            BookName = bookName;
+            Price = price;
+            Category = category;
+            Author = author;    
+        }
+    }
+   
+}
